bdb19eb9116e4d81b1ed21ac1909322ba8133895
zhichuanxun <zhichuanxun@gmail.com>
2019-07-08 10:09:42 +0800

    test update

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo apt-get update
-sudo apt -y install nodejs
-sudo apt -y install default-jdk
+sudo su
+apt-get update
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+az login -u zhichuanxun@outlook.com -p qaz123qaz
+touch test.txt
+az group list > test.txt
